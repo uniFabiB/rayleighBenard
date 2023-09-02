236a14e9da9d681a9a3e065da68eeac14cc4859f
uniFabiB <78479491+uniFabiB@users.noreply.github.com>
2023-09-02 15:20:11 +0200

    Add files via upload

--- rb.py
+++ rb.py
@@ -2,7 +2,6 @@ import os
 os.environ["OMP_NUM_THREADS"] = "1"
 
 import myUtilities
-from myMeshes import *
 from firedrake import *
 import datetime
 import weakref
@@ -18,23 +17,25 @@ utils.generateRecoveryScript(__file__)
 
 
 ### PARAMETERS ###
-n = 512
+nXY = 256
 
-nOut = n
+nOut = nXY
 
-tau = 0.00001
-writeOutputEveryXsteps = 20
-writeOutputEvery = tau*writeOutputEveryXsteps			# write mesh functions
-writeUP = True						# output u and p?
+uSpace = "Lag"			# either Hdiv or Lag
 
-Lx = 1.0
+dt = 10.0**(-6)
+writeOutputEveryXsteps = 1
+writeOutputEvery = dt*writeOutputEveryXsteps			# write mesh functions
+writeUP = False						# output u and p?
+
+Lx = 2.0
 Ly = 1.0
 
 t = 0.0
-tEnd = 1.0
+tEnd = 100 #1.0
 
 ### only nav slip ###
-Ls = 0.01			# alpha = 2/L_s in navier slip as Du = 1/2 partial_1 u_2 on top and bottom
+alpha = 100000.0		# alpha in tau Du n = alpha tau u
 
 				
 nu = 1.0			# ... - nu * Laplace u ...
@@ -42,23 +43,20 @@ kappa = 1.0			# ... - kappa * Laplace theta ...
 Ra = 10.0**8			# ... + Ra * theta * e_2
 Pr = 1.0			# 1/Pr*(u_t+u cdot nabla u) + ...
 
-meshTyp = "myMeshHigherOrder"				#"periodicRectangle", "myMesh", "myMeshHigherOrder"
+ampFreqOffsetList = [[0.15,1.0,0]] # examples: [[0.1,1.0,0.0],[0.1,2.0,0.0]] or [[0.1,1.0,pi/2.0]]
 meshDiagonal = "crossed"			# "left"= /	"right"= \	crossed = "X"
 
-slip = "noSlip"		# "noSlip"	"navSlip"
-
-problem = "rayleighBenard"		# "boussinesq"	"rayleighBenard"
 ### PARAMETERS END ###
 
 
 ### OPTIONS ###
-projectPoutputToAverageFree = True 	# force the output function of p to be average free (doesn't change the calculation)
+projectPoutputToAverageFree = False 	# force the output function of p to be average free (doesn't change the calculation)
 
 dataFolder = outputFolder + "data/"
 
 
-nx = round(n*Lx/Ly)
-ny = n
+nx = round(nXY*Lx/Ly)
+ny = nXY
 
 nxOut = round(nOut*Lx/Ly)
 nyOut = nOut
@@ -69,61 +67,44 @@ printErrors = True
 
 #nCells = nx*ny*2	for diagonal "left" or "right"
 #nCells = nx*ny*4	for diagonal "crossed"
-utils.print("n / core ", abs(sqrt(nx*ny/COMM_WORLD.size)))
+nPerCore = abs(sqrt(nx*ny/COMM_WORLD.size))
+utils.print("n / core ", nPerCore)
 
-utils.putInfoInInfoString("n",n)
-utils.putInfoInInfoString("tau",tau)
+utils.putInfoInInfoString("nXY",nXY)
+utils.putInfoInInfoString("n/core",nPerCore)
+utils.putInfoInInfoString("dt",dt)
 utils.putInfoInInfoString("Lx",Lx)
 utils.putInfoInInfoString("Ly",Ly)
 utils.putInfoInInfoString("tEnd",tEnd)
-utils.putInfoInInfoString("Ls",Ls)
+utils.putInfoInInfoString("alpha",alpha)
 utils.putInfoInInfoString("kappa",kappa)
 utils.putInfoInInfoString("Ra",Ra)
-utils.putInfoInInfoString("problem",problem)
-utils.putInfoInInfoString("meshTyp",meshTyp)
 utils.putInfoInInfoString("projectPoutputToAverageFree",projectPoutputToAverageFree)
 utils.putInfoInInfoString("dataFolder",dataFolder)
 
 
 
+### mesh ###
+mesh = PeriodicRectangleMesh(nx,ny,Lx,Ly, "x", comm = comm, diagonal = meshDiagonal)	# mesh Lx=Gamma in e_1, Ly in e_2, periodic in x=e_1 dir
+boundary_id_bot = 1
+boundary_id_top = 2
 
-if meshTyp == "periodicRectangle":
-	mesh = PeriodicRectangleMesh(nx,ny,Lx,Ly, "x")	# mesh Lx=Gamma in e_1, Ly in e_2, periodic in x=e_1 dir
-	boundary_id_bot = 1
-	boundary_id_top = 2
-elif meshTyp == "myMesh":
-	meshAmpl = 0.02
-	meshFreq = 6.0
-	meshPhi0 = 0.0
-	mesh = myPartiallyPeriodicRectangleMesh(nx, ny, Lx, Ly, meshAmpl, meshFreq, meshPhi0, comm = comm, diagonal = meshDiagonal)
-	meshOut = myPartiallyPeriodicRectangleMesh(nxOut, nyOut, Lx, Ly, meshAmpl, meshFreq, meshPhi0, comm = comm, diagonal = meshDiagonal)
-	#kappa = gamma''/(1+gamma'^2)^(3/2)		#https://de.wikipedia.org/wiki/Kr%C3%BCmmung#Ebene_Kurven
-	# gamma(x_1)= A sin(2pi freq x_1/Gamma)
-	# -> kappa(x_1)=-A (2 pi freq/Gamma)^2 sin(2 pi freq/Gamma x_1) / (1+ ( A 2 pi freq/Gamma)^2 cos^2(2pi freq x_1/Gamma)) 
-	# max |kappa| = A (2 pi freq/Gamma)^2 = meshAmpl *(2.0 * pi * meshFreq/Lx)**2
-	maxKappa = meshAmpl *(2.0 * pi * meshFreq/Lx)**2
-	utils.print("maxKappa",maxKappa)
-	utils.putInfoInInfoString("maxKappa",maxKappa)
-	boundary_id_bot = 1
-	boundary_id_top = 2
-	boundary_ids = (boundary_id_bot, boundary_id_top)
-elif meshTyp == "myMeshHigherOrder":
-	meshAmplBotList = [0.01, 0.01, 0.01]
-	meshFreqBotList = [8.0, 2.0, 1.0]
-	meshPhi0BotList = [0.0, 0.0, 0.0]
-	meshAmplTopList = meshAmplBotList
-	meshFreqTopList = meshFreqBotList
-	meshPhi0TopList = meshPhi0BotList
-	mesh = myHigherOrderNonSymmetricPartiallyPeriodicRectangleMesh(nx, ny, Lx, Ly, meshAmplBotList, meshFreqBotList, meshPhi0BotList, meshAmplTopList, meshFreqTopList, meshPhi0TopList, comm = comm, diagonal = meshDiagonal)
-	meshOut = myHigherOrderNonSymmetricPartiallyPeriodicRectangleMesh(nxOut, nyOut, Lx, Ly, meshAmplBotList, meshFreqBotList, meshPhi0BotList, meshAmplTopList, meshFreqTopList, meshPhi0TopList, comm = comm, diagonal = meshDiagonal)
-	
-	#maxKappa = max(meshAmplBot *(2.0 * pi * meshFreqBot/Lx)**2, meshAmplTop *(2.0 * pi * meshFreqTop/Lx)**2)
-	boundary_id_bot = 1
-	boundary_id_top = 2
-	boundary_ids = (boundary_id_bot, boundary_id_top)
-else:
-        raise Error("ERROR mesh not specified")
-        
+# change y variable
+Vc = mesh.coordinates.function_space()
+x, y = SpatialCoordinate(mesh)
+#for ampFreqOffset in ampFreqOffsetList:
+#	amp = ampFreqOffset[0]
+#	freq = ampFreqOffset[1]
+#	offset = ampFreqOffset[2]	
+#	y = y + amp * sin((2*pi*freq*x+offset)/Lx)
+amp = 0.02
+freq = 1
+freqSin = 3
+freqCos = 8
+offset = 0.4*Lx
+y = y + amp * sin(2*pi*freq*(x-offset)/Lx+freqSin*sin(2*pi*freq*(x-offset)/Lx)+freqCos*cos(2*pi*freq*(x-offset)/Lx))
+f = Function(Vc).interpolate(as_vector([x, y]))
+mesh.coordinates.assign(f)
 
 
 
@@ -132,16 +113,21 @@ utils.writeInfoFile()
 
 
 
-normal = FacetNormal(mesh)
-tangential = as_vector((-normal[1],normal[0]))
+n = FacetNormal(mesh)
+tau = as_vector((-n[1],n[0]))
 x,y = SpatialCoordinate(mesh)
 
 
 
 
-V_u = VectorFunctionSpace(mesh, "CG", 2)
-V_p = FunctionSpace(mesh, "CG", 1)
-V_t = FunctionSpace(mesh, "CG", 1)
+order = 1
+if uSpace == "Hdiv":
+	V_u = FunctionSpace(mesh, "RT", order+1)
+elif uSpace == "Lag":
+	V_u = VectorFunctionSpace(mesh, "CG", order+1)
+V_p = FunctionSpace(mesh, "CG", order)
+V_t = FunctionSpace(mesh, "CG", order)
+
 
 Z = V_u * V_p * V_t
 
@@ -160,149 +146,221 @@ thetaOld = Function(V_t)
 
 
 nu = Constant(float(nu))
-Ls = Constant(float(Ls))
 kappa = Constant(float(kappa))
 Ra = Constant(float(Ra))
 Pr = Constant(float(Pr))
 
+Du = 0.5*(grad(u)+nabla_grad(u))
+Dv = 0.5*(grad(v)+nabla_grad(v))
+v_n = dot(n,v)*n
+v_tau = dot(tau,v)*tau
+u_n = dot(n,u)*n
+u_tau = dot(tau,u)*tau
 
+#F_bsqBackward_navSlip = (
+#	inner(u-uOld,v)*dx
+#	+ dt*(
+#		inner(dot(u, nabla_grad(u)), v)*dx
+#		- inner(p,div(v))*dx
+#		+ 2.0 * inner(Du, Dv)*dx
+#		- Ra * inner(theta,v[1])*dx
+#		+ 2.0 * alpha * inner(u,v_tau)*ds
+#	)
+#	+ inner(theta-thetaOld,s)*dx
+#	+ dt*( 
+#		kappa * inner(grad(theta), grad(s))*dx
+#		+ inner(dot(u,grad(theta)),s)*dx
+#	)
+#	+ inner(div(u),q)*dx
+#)
 
-F_crankNicolson_navSlip = (
-	1.0/Pr * inner(u-uOld,v)*dx
-	+ tau*(
-		 1.0/Pr * 1.0/2.0*(inner(dot(u, nabla_grad(u)), v)+inner(dot(uOld, nabla_grad(uOld)), v))*dx
-		+ inner(grad(p),v)*dx
-		+ nu * 1.0/2.0*(inner(grad(u), grad(v))+inner(grad(uOld), grad(v)))*dx
-		+ nu * 1.0/Ls * 1.0/2.0*(inner(u[0], v[0])+inner(uOld[0], v[0]))*ds
-		- Ra * 1.0/2.0*(inner(theta,v[1]) + inner(thetaOld,v[1]))*dx
-	)
-	+ inner(theta-thetaOld,s)*dx
-	+ tau*( 
-		kappa * 1.0/2.0*(inner(grad(theta), grad(s))+inner(grad(thetaOld), grad(s)))*dx
-		+ 1.0/2.0*(inner(dot(u,grad(theta)),s)+inner(dot(uOld,grad(thetaOld)),s))*dx
-	)
-	+ inner(u,grad(q))*dx
-)
-
-
-F_crankNicolson_noSlip = (
-	1.0/Pr * inner(u-uOld,v)*dx
-	+ tau*(
-		 1.0/Pr * 1.0/2.0*(inner(dot(u, nabla_grad(u)), v)+inner(dot(uOld, nabla_grad(uOld)), v))*dx
-		+ inner(grad(p),v)*dx
-		+ nu * 1.0/2.0*(inner(grad(u), grad(v))+inner(grad(uOld), grad(v)))*dx
-		- Ra * 1.0/2.0*(inner(theta,v[1]) + inner(thetaOld,v[1]))*dx
-	)
-	+ inner(theta-thetaOld,s)*dx
-	+ tau*( 
-		kappa * 1.0/2.0*(inner(grad(theta), grad(s))+inner(grad(thetaOld), grad(s)))*dx
-		+ 1.0/2.0*(inner(dot(u,grad(theta)),s)+inner(dot(uOld,grad(thetaOld)),s))*dx
-	)
-	+ inner(u,grad(q))*dx
-)
-
-F_bsqBackward_navSlip = (
-	1.0/Pr * inner(u-uOld,v)*dx
-	+ tau*(
-		 1.0/Pr * inner(dot(u, nabla_grad(u)), v)*dx
-		+ inner(grad(p),v)*dx
-		+ nu * inner(grad(u), grad(v))*dx
-		+ nu * 1.0/Ls * inner(u[0], v[0])*ds
+F_rb_backwards = (
+	inner(u-uOld,v)*dx
+	+ dt*(
+		inner(dot(u,nabla_grad(u)),v)*dx
+		- inner(p,div(v))*dx
+		+ nu * 2.0 * inner(Du, grad(v))*dx
 		- Ra * inner(theta,v[1])*dx
+		+ alpha * nu * 2.0 * inner(u,v)*ds
 	)
+	
 	+ inner(theta-thetaOld,s)*dx
-	+ tau*( 
-		kappa * inner(grad(theta), grad(s))*dx
-		+ inner(dot(u,grad(theta)),s)*dx
+	+ dt*(
+		inner(dot(u,grad(theta)),s)*dx
+		+ kappa * inner(grad(theta),grad(s))*dx
+#?!?!?!!?!	#- kappa * inner(dot(n,grad(theta)),s)*ds
+
 	)
+	
 	+ inner(u,grad(q))*dx
 )
 
-F_bsqBackward_noSlip = (
-	1.0/Pr * inner(u-uOld,v)*dx
-	+ tau*(
-		 1.0/Pr * inner(dot(u, nabla_grad(u)), v)*dx
+F_rb_backwards_noSlip = (
+	inner(u-uOld,v)*dx
+	+ dt*(
+		inner(dot(u,nabla_grad(u)),v)*dx
 		+ inner(grad(p),v)*dx
 		+ nu * inner(grad(u), grad(v))*dx
 		- Ra * inner(theta,v[1])*dx
 	)
+	
 	+ inner(theta-thetaOld,s)*dx
-	+ tau*( 
-		kappa * inner(grad(theta), grad(s))*dx
-		+ inner(dot(u,grad(theta)),s)*dx
+	+ dt*(
+		inner(dot(u,grad(theta)),s)*dx
+		+ kappa * inner(grad(theta),grad(s))*dx
+#?!?!?!!?!	#- kappa * inner(dot(n,grad(theta)),s)*ds
+
+	)
+	
+	+ inner(u,grad(q))*dx
+)
+
+
+F_rb_crankNicolson_noSlip = (
+	1.0/Pr * inner(u-uOld,v)*dx
+	+ dt*(
+		1.0/Pr * 1.0/2.0*(inner(dot(u, nabla_grad(u)), v)+inner(dot(uOld, nabla_grad(uOld)), v))*dx
+		+ inner(grad(p),v)*dx
+		+ nu * 1.0/2.0*(inner(grad(u), grad(v))+inner(grad(uOld), grad(v)))*dx
+		- Ra * 1.0/2.0*(inner(theta,v[1]) + inner(thetaOld,v[1]))*dx
+	)
+	+ inner(theta-thetaOld,s)*dx
+	+ dt*( 
+		1.0/2.0*(inner(dot(u,grad(theta)),s)+inner(dot(uOld,grad(thetaOld)),s))*dx
+		+ kappa * 1.0/2.0*(inner(grad(theta), grad(s))+inner(grad(thetaOld), grad(s)))*dx
+#?!?!?!!?!	#- kappa * 1.0/2.0*(inner(dot(n,grad(theta)),s)+inner(dot(n,grad(thetaOld)),s))*ds
 	)
 	+ inner(u,grad(q))*dx
 )
 
-if problem == "boussinesq":
-	if slip == "noSlip":
-		scheme = "backward euler noSlip"
-		F = F_bsqBackward_noSlip
-	elif slip == "navSlip":
-		scheme = "backward euler navSlip"
-		F = F_bsqBackward_navSlip
-	else:
-		utils.warn("slip (",slip,") is not correct")
-elif problem == "rayleighBenard":
-	if slip == "noSlip":
-		scheme = "crank nicolson noSlip"
-		F = F_crankNicolson_noSlip
-	elif slip == "navSlip":
-		scheme = "crank nicolson navSlip"
-		F = F_crankNicolson_navSlip
-	else:
-		utils.warn("slip (",slip,") is not correct")
-else:
-	utils.warn("problem not correct specified")
 
-if slip == "navSlip" and meshTyp == "myMesh":
-	utils.warn("navSlip does not work with curved mesh!!!")
+
+# u_t + u cdot nabla u + nabla p - sqrt(Pr/Ra) Delta u = theta e_2
+# theta_t + u cdot nabla theta - 1/sqrt(Pr*Ra) Delta theta = 0
+# seems to yield any improvement over the other method (except automatic rescaling of time)
+# doesn't converge for Ra 10^8 and n = 128 after some time
+F_rb_PadbergGehle_backwards_noSlip = (
+	inner(u-uOld,v)*dx
+	+ dt*(
+		inner(dot(u,nabla_grad(u)),v)*dx
+		+ inner(grad(p),v)*dx
+		+ sqrt(Pr/Ra) * inner(grad(u), grad(v))*dx
+		- inner(theta,v[1])*dx
+	)
 	
+	+ inner(theta-thetaOld,s)*dx
+	+ dt*(
+		inner(dot(u,grad(theta)),s)*dx
+		+ 1.0/(sqrt(Pr*Ra)) * inner(grad(theta),grad(s))*dx
+#?!?!?!!?!	#- kappa * inner(dot(n,grad(theta)),s)*ds
+
+	)
 	
-utils.putInfoInInfoString("scheme",scheme)
+	+ inner(u,grad(q))*dx
+)
+
+
+
+
+
+# - int Delta u v = int_{\partial\Omega} (2alpha+kappa) u v + \langle nabla u, nabla v \rangle -> kappa=0 because of grid?!
+F_test = (
+	inner(u-uOld,v)*dx
+	+ dt*(
+		inner(dot(u,nabla_grad(u)),v)*dx
+		- inner(p,div(v))*dx
+		+ nu * inner(grad(u), grad(v))*dx
+		- Ra * inner(theta,v[1])*dx
+		+ alpha * nu * 2.0 * inner(u,v)*ds
+	)
+	
+	+ inner(theta-thetaOld,s)*dx
+	+ dt*(
+		inner(dot(u,grad(theta)),s)*dx
+		+ kappa * inner(grad(theta),grad(s))*dx
+#?!?!?!!?!	#- kappa * inner(dot(n,grad(theta)),s)*ds
+
+	)
+	
+	+ inner(u,grad(q))*dx
+)
+
+
+
+
+F = F_rb_PadbergGehle_backwards_noSlip
+
 
 # initial conditions for u
 u = project(Constant([0.0,0.0]), V_u)
 uOld.assign(u)
 
 
-if problem == "boussinesq":
-	icTheta = 1.0*sin(pi*y/Ly+2*pi*x/Lx)*sin(2*pi*x/Lx+2*pi*y/Ly)
-	#icTheta = 1.0*exp(-(1.0*(y-0.5*Ly)**2))*sin(2*pi*x/Lx)
-	theta = project(icTheta, V_t)
-elif problem == "rayleighBenard":
-	icTheta1 = 0.001*exp(-(10.0*(y-0.5*Ly)**2))*sin(2*pi*x/Lx)
-	icTheta2 = 0.0001*exp(-(10.0*(y-0.5*Ly)**2))*exp(-(10.0*(x/2.0-0.5*Lx)**2))
-	theta = project(icTheta1+icTheta2, V_t)
-#	theta = project(Constant(0.0), V_t)
-else:
-	utils.warn("problem not correct specified") 
-
+x, y = SpatialCoordinate(mesh)
+#icTheta = 0.5*sin(2*pi*x/Lx)*exp(-4*(0.5-y)**2)+0.5
+icTheta = Constant(0.5)
+theta = project(icTheta, V_t)
 thetaOld.assign(theta)
 
 bcs = []
 
-if problem == "rayleighBenard":
-	bc_rbBot = DirichletBC(Z.sub(2), Constant(1.0), (boundary_id_bot))
-	bc_rbTop = DirichletBC(Z.sub(2), Constant(-1.0), (boundary_id_top))
-	bcs.append(bc_rbBot)
-	bcs.append(bc_rbTop)
+bc_rbBot = DirichletBC(Z.sub(2), Constant(1.0), (boundary_id_bot))
+bc_rbTop = DirichletBC(Z.sub(2), Constant(0.0), (boundary_id_top))
+bcs.append(bc_rbBot)
+bcs.append(bc_rbTop)
 
-if slip == "noSlip":
-	bc_noSlip = DirichletBC(Z.sub(0), Constant([0.0, 0.0]), "on_boundary")
-	bcs.append(bc_noSlip)
-elif slip == "navSlip":
-	bc_noPen = DirichletBC(Z.sub(0).sub(1), Constant(0.0), "on_boundary")
-	bcs.append(bc_noPen)
+
+u_n = dot(u,n)
+v_n = dot(v,n)
+
+
+
+
+#bc_u = EquationBC(inner(dot(n,Du) - alpha*u,v_tau)*ds+inner(u,v_n)*ds==0, u, (1,2), V=Z.sub(0))
+#bc_uN = EquationBC(inner(dot(u,n),dot(v,n))*ds==0, u, (1,2), V=Z.sub(0))
+#bc_uTau = EquationBC(inner(u,v_tau)*ds==0, u, (1,2), V=Z.sub(0))
+#bc_uNav = EquationBC(inner(dot(Du,n)-alpha*u,v)*ds==0, u, (1,2), V=Z.sub(0), bcs=bc_uN)
+#navSlipBc = EquationBC(inner(dot(n,Du)+alpha*u,v_tau)*ds+inner(u,v_n)*ds==0, u, "on_boundary", V=Z.sub(0))
+
+
+#bc_t1 = EquationBC(inner(u+0.00005*dot(n,Du),v_tau)*ds+inner(u,v_n)*ds==0, u, (1,2), V=Z.sub(0))
+#bc_un0 = EquationBC(inner(dot(u,n),dot(v,n))*ds==0, u, (1,2), V=Z.sub(0))
+#bc_t3 = EquationBC(inner(dot(dot(grad(u)+nabla_grad(u),n)+alpha*u,tau),dot(v,tau))*ds==0, u, (1,2), V=Z.sub(0), bcs=bc_un0)
+#bc_t4 = EquationBC(inner(u,v)*ds==0, u, (1,2), V=Z.sub(0))
+#bc_utau0 = EquationBC(inner(u,v_tau)*ds==0, u, (1,2), V=Z.sub(0))
+#bc_t6 = EquationBC(inner(u,v_tau)*ds+inner(u,v_n)*ds==0, u, (1,2), V=Z.sub(0))
+
+if uSpace == "Hdiv":
+	bc_noPenHdiv = DirichletBC(Z.sub(0), Constant((0.0,0.0)), (boundary_id_bot,boundary_id_top)) # because of the hdiv space setting 0 bc for it is only setting u cdot n = 0 #https://github.com/firedrakeproject/firedrake/issues/169#issuecomment-34557942
+	bcs.append(bc_noPenHdiv)
+elif uSpace == "Lag":
+	bcs_noSlip = DirichletBC(Z.sub(0), Constant((0.0,0.0)), (boundary_id_bot,boundary_id_top))
+	bcs.append(bcs_noSlip)
+	#bc_uN = EquationBC(inner(dot(u,n),dot(v,n))*ds==0, u, (boundary_id_bot,boundary_id_top), V=Z.sub(0))
+	#bc_uN_nav = EquationBC(inner(dot(tau,dot(Du,n))+alpha*dot(u,tau),dot(v,tau))*ds==0, u, (boundary_id_bot,boundary_id_top), V=Z.sub(0), bcs=bc_uN)
+	#bc_uN_nav = EquationBC(inner(dot(tau,dot(Du,n))+alpha*dot(u,tau),dot(v,tau))*ds+inner(dot(u,n),dot(v,n))*ds==0, u, (boundary_id_bot,boundary_id_top), V=Z.sub(0))
+	#bcs.append(bc_uN_nav)
 	
+
 	
 
 problem = NonlinearVariationalProblem(F, upt, bcs = bcs)
 
 
 nullspace = MixedVectorSpaceBasis(Z, [Z.sub(0), VectorSpaceBasis(constant=True), Z.sub(2)])
-solver = NonlinearVariationalSolver(problem, nullspace = nullspace)
 
+
+parameters_easySplit = {	# splits the solving of the nse and the temperature equation and nothing else
+	# seems to be more robust but is super slow on big grids
+	"pc_type": "fieldsplit",
+	"pc_fieldsplit_type": "multiplicative",		# additive multiplicative symmetric_multiplicative special schur gkb
+	"pc_fieldsplit_0_fields": "0,1",
+	"pc_fieldsplit_1_fields": "2",
+}
+
+
+#solver = NonlinearVariationalSolver(problem, nullspace = nullspace, solver_parameters=parameters_easySplit)
+solver = NonlinearVariationalSolver(problem, nullspace = nullspace)
 
 uptFile = File(dataFolder+"upt.pvd", comm = COMM_WORLD)
 lastWrittenOutput = -1
@@ -319,8 +377,8 @@ def writeMeshFunctions():
 	if nxOut != nx or nyOut != ny:
 		thetaOut = project(theta, V_ptOut)
 	else:
-		thetaOut = theta
-		
+#		thetaOut = theta
+		thetaOut = project(theta, V_ptOut)		
 	thetaOut.rename("theta")
 	if writeUP:
 		if nxOut != nx or nyOut != ny:
@@ -345,7 +403,7 @@ p = Function(V_p)
 p = project(Constant(0.0), V_p)
 
 
-
+meshOut = mesh
 meshOut._parallel_compatible = {weakref.ref(mesh)}
 
 V_uOut = VectorFunctionSpace(meshOut, "CG", 1)
@@ -361,34 +419,30 @@ utils.print("starting to solve")
 
 
 
+
+
 tWorld = datetime.datetime.now()
 while(t<tEnd):
 	
 	solver.solve()
-	t = t + tau
+	
+	
+	t = t + dt
 	utils.setSimulationTime(t)
-	u, p, theta = upt.split()
+	u, p, theta = upt.subfunctions		# depending on the firedrake version might have to use upt.split()
 	uOld.assign(u)
 	thetaOld.assign(theta)
 	
 	if round(t,12) >= round(lastWrittenOutput + writeOutputEvery,12):
 		writeMeshFunctions()
+	#utils.print("u\t",assemble(inner(u,u)*ds))	
+	#utils.print("u tau\t",assemble(inner(u,tau)*ds))	
+	#utils.print("u n\t",assemble(inner(u,n)*ds))	
+	#utils.print("n Du tau\t",assemble(inner(dot(n,Du),tau)*ds))
+	#utils.print("temp\t",assemble(inner(alpha*u+dot(n,Du),tau)*ds))
 	
-	utils.print(round(t/tEnd*100,12),"% done (after",datetime.datetime.now()-tWorld,"), t=",round(t,12))
-	if printErrors:
-		utils.print("\tl2 div(u) / l2 grad(u)", round(norm(div(u))/norm(grad(u)),12))
-		expression1 = grad(u)[0,1]+1.0/Ls*u[0]
-		expression2 = grad(u)[0,1]-1.0/Ls*u[0]
-		bdryError1 = abs(sqrt(assemble(inner(expression1,expression1)*ds(boundary_id_top))))
-		bdryError2 = abs(sqrt(assemble(inner(expression2,expression2)*ds(boundary_id_bot))))
-		bdryError = bdryError1+bdryError2
-		utils.print("\tl2 partial_2 u_1 + 1/Ls u_1", bdryError)			#grad(v)[i,j] = v[i].dx(j)		nabla_grad(v)[i,j] = v[j].dx(i)
-		LsU1 = abs(sqrt(assemble(inner(1.0/Ls*u[0],1.0/Ls*u[0])*ds)))
-		utils.print("\tl2 u_1", LsU1)
-		partial2U1 = abs(sqrt(assemble(inner(grad(u)[0,1],grad(u)[0,1])*ds)))
-		utils.print("\tl2 partial_2 u_1", partial2U1)
-		if LsU1+partial2U1 != 0:
-			utils.print("\tls Du + au / (l2 u1 + l2 partial_2 u_1)", round(bdryError/(LsU1+partial2U1),12))
+	
+	utils.print(round(t/tEnd*100,12),"% done (after",datetime.datetime.now()-tWorld,"), t=",round(t,9))
 	tWorld = datetime.datetime.now()
 utils.writeEndInfo()
 
